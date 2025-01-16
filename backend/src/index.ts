78f51c9050d21f10abcd9b8445f80e19696f42d1
Anil Parajuli <anilparajuli998@gmail.com>
2025-01-16 13:03:15 +0545

    completing diployment process

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
 import orderRoute from "./routes/OrderRoute";
+import path from "path";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -19,6 +20,8 @@ cloudinary.config({
 });
 
 const app = express();
+
+const _direname = path.resolve();
 
 app.use(cors());
 
@@ -34,6 +37,11 @@ app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
 app.use("/api/order", orderRoute);
+
+app.use(express.static(path.join(_direname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(_direname, "frontend", "dist", "index.html"));
+});
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
